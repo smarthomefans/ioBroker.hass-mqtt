cf2d35f90d141b00df12e74fd9cc03faba30ecca
SchumyHao <schumyhaojl@126.com>
2019-04-13 12:20:28 +0800

    "Initial commit"

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "hass-mqtt adapter settings": {
+        "en": "Adapter settings for hass-mqtt",
+        "de": "Adaptereinstellungen für hass-mqtt",
+        "ru": "Настройки адаптера для hass-mqtt",
+        "pt": "Configurações do adaptador para hass-mqtt",
+        "nl": "Adapterinstellingen voor hass-mqtt",
+        "fr": "Paramètres d'adaptateur pour hass-mqtt",
+        "it": "Impostazioni dell'adattatore per hass-mqtt",
+        "es": "Ajustes del adaptador para hass-mqtt",
+        "pl": "Ustawienia adaptera dla hass-mqtt",
+        "zh-cn": "hass-mqtt的适配器设置"
+    },
+    "option1": {
+        "en": "option1",
+        "de": "Option 1",
+        "ru": "Опция 1",
+        "pt": "Opção 1",
+        "nl": "Optie 1",
+        "fr": "Option 1",
+        "it": "opzione 1",
+        "es": "Opción 1",
+        "pl": "opcja 1",
+        "zh-cn": "选项1"
+    },
+    "option2": {
+        "en": "option2",
+        "de": "Option 2",
+        "ru": "option2",
+        "pt": "opção 2",
+        "nl": "Optie 2",
+        "fr": "Option 2",
+        "it": "opzione 2",
+        "es": "opcion 2",
+        "pl": "Opcja 2",
+        "zh-cn": "选项2"
+    }
+};
